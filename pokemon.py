b95aadc18f70c5b81b70ac9244193ab0d3ba6a7f
glmdgrielson <zapperchamp1@gmail.com>
2021-12-16 15:18:06 -0500

    Add a data structure, more or less

---
+++ pokemon.py
@@ -0,0 +1,11 @@
+"""Create a basic data structure for a Pokémon's stats.
+"""
+
+class PocketMonster(object):
+    """A Pokémon.
+    """
+    # TODO: figure out what sort of data I should pass in.
+    def __init__(self, *args):
+        """Create a Pokémon.
+        """
+        pass
