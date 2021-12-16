a131bc4f8a9f7b30c2253ea9a61c745fa12178e5
glmdgrielson <zapperchamp1@gmail.com>
2021-12-16 17:18:33 -0500

    Begin the process of data entry

--- pokemon.py
+++ pokemon.py
@@ -1,11 +1,25 @@
 """Create a basic data structure for a Pokémon's stats.
 """
 
+from pokegen import NATURES, generate_nature
+import json
+
 class PocketMonster(object):
     """A Pokémon.
     """
+    health = 1
+    attack = 1
+    defense = 1
+    sp_attack = 1
+    sp_defense = 1
     # TODO: figure out what sort of data I should pass in.
-    def __init__(self, *args):
+    def __init__(self, species, level, *args):
         """Create a Pokémon.
+
+        Arguments:
+            species: What species this is. This is expecting a string,
+            not a dex number.
+            level: the current level of this Pokémon.
         """
         pass
+
