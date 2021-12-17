17b2cc4a9275743f50cf1fe59f5256a3e26d5343
glmdgrielson <zapperchamp1@gmail.com>
2021-12-16 19:52:09 -0500

    More data processing

--- pokemon.py
+++ pokemon.py
@@ -12,6 +12,8 @@ class PocketMonster(object):
     defense = 1
     sp_attack = 1
     sp_defense = 1
+    speed = 1
+    gender = None # type: Union[str, None]
     # TODO: figure out what sort of data I should pass in.
     def __init__(self, species, level, *args):
         """Create a Pokémon.
@@ -20,6 +22,11 @@ class PocketMonster(object):
             species: What species this is. This is expecting a string,
             not a dex number.
             level: the current level of this Pokémon.
+        """
+        pass
+
+    def apply_damage(self, damage, typing, category):
+        """Apply damage, for GMing purposes.
         """
         pass
 
